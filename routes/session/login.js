33a8da838f072b429e232b963b9fc0f5fc543676
spetsing <spetsing@mit.edu>
2019-01-30 09:00:03 -0500

    Updating response

--- routes/session/login.js
+++ routes/session/login.js
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
         });
     }).catch((err) => {
         console.log(err);
-        res.status(400).send(err);
+        res.status(400).json(err);
     });
 
 };
